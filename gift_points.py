75202a2ded6e040be6d6c22d5ea005d202153581
preshina <ojhapreshina@gmail.com>
2026-03-20 11:21:41 +0545

    updated gift points logic

--- gift_points.py
+++ gift_points.py
@@ -7,3 +7,29 @@
 # Add amount to each friend’s points
 
 # If not enough points, print "Not enough points for gifting" and stop gifting.
+
+
+class Player:
+    def __init__(self, name, points):
+        self.name = name
+        self.points = points
+
+    def gift_points(self, amount, friends_list):
+        total_needed = amount * len(friends_list)
+
+        if self.points < total_needed:
+            print("Not enough points for gifting")
+            return
+
+        self.points -= total_needed
+
+        for friend in friends_list:
+            friend.points += amount
+
+
+p1 = Player("Hari", 100)
+p2 = Player("Ram", 10)
+p3 = Player("Sita", 5)
+
+p1.gift_points(20, [p2, p3])
+print(p1.points, p2.points, p3.points)
